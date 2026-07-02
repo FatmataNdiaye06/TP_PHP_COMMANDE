f7e58dd4d26b2570909ebc1c0abaf1963c460fca
Fatmata06 <nfatmata614@gmail.com>
2026-07-02 05:36:00 +0000

    feat: Enregistrer un client

--- index.php
+++ index.php
@@ -1,10 +1,18 @@
 <?php
 require(__DIR__."/view/readline.view.php");
+
 require(__DIR__."/utils/validator.utils.php");
 require(__DIR__."/utils/error.utils.php");
+
 require(__DIR__."/controller/pruduct.controller.php");
+require(__DIR__."/controller/client.controller.php");
+
 require(__DIR__."/model/pruduct.model.php");
+require(__DIR__."/model/client.model.php");
+
 require(__DIR__."/service/pruduct.service.php");
+require(__DIR__."/service/client.service.php");
+
 
 
 
@@ -18,5 +26,17 @@ RG3: Le prix et quantité sont positifs
  */
 // feat2: Archiver un produit
 // feat3: Lister les produits non archivés ou archivés
-saveProduct();
-var_dump($products);
+
+
+
+
+// Increment2: Gestion des clients
+// feat1: Enregistrer un client
+    /*
+    RG1: telephone est  obligatoire et unique
+    RG2: la nom prenom  est  obligatoire
+    */
+// feat2: Lister les clients qui n'ont pas de commandes
+// saveProduct();
+ajouterClientAction($clients);
+// var_dump($products);
